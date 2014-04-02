e5c64cc1fa6953738b3c47fdf9fbd4166dbb7ca9
Kevin Woo <kevinawoo@gmail.com>
2014-04-01 23:46:32 -0700

    changed array names

--- data/front.php
+++ data/front.php
@@ -1,61 +1,61 @@
 <?php
 
-$data = array();
+$front = array();
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 1',
-  'start' => '2012-04-20', 
+  'start' => '2012-04-20',
   'end'   => '2012-05-12'
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 2',
-  'start' => '2012-04-22', 
+  'start' => '2012-04-22',
   'end'   => '2012-05-22'
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 3',
-  'start' => '2012-05-25', 
+  'start' => '2012-05-25',
   'end'   => '2012-06-20'
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 4',
-  'start' => '2012-05-06', 
+  'start' => '2012-05-06',
   'end'   => '2012-06-17',
   'class' => 'important',
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 5',
-  'start' => '2012-05-11', 
-  'end'   => '2012-06-03', 
+  'start' => '2012-05-11',
+  'end'   => '2012-06-03',
   'class' => 'urgent',
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 6',
-  'start' => '2012-05-15', 
+  'start' => '2012-05-15',
   'end'   => '2012-07-03'
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 7',
-  'start' => '2012-06-01', 
-  'end'   => '2012-07-03', 
+  'start' => '2012-06-01',
+  'end'   => '2012-07-03',
   'class' => 'important',
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 8',
-  'start' => '2012-06-01', 
+  'start' => '2012-06-01',
   'end'   => '2012-08-05'
 );
 
-$data[] = array(
+$front[] = array(
   'label' => 'Project 9',
-  'start' => '2012-07-22', 
+  'start' => '2012-07-22',
   'end'   => '2012-09-05',
   'class' => 'urgent',
 );
