7e3275a4d2517efc6eeaca16df420813860f138d
Lashan Dias <diaslashan@gmail.com>
2014-04-02 08:05:09 -0700

    added front end gantt info

--- data/front.php
+++ data/front.php
@@ -3,61 +3,39 @@
 $front = array();
 
 $front[] = array(
-  'label' => 'Project 1',
-  'start' => '2014-04-20',
-  'end'   => '2014-05-12'
+  'label' => 'Search Pages (2)',
+  'start' => '2014-1-15',
+  'end'   => '2014-4-02'
 );
 
 $front[] = array(
-  'label' => 'Project 2',
-  'start' => '2014-04-22',
-  'end'   => '2014-05-22'
+  'label' => 'Product Pages (3)',
+  'start' => '2014-3-15',
+  'end'   => '2014-4-15'
 );
 
 $front[] = array(
-  'label' => 'Project 3',
-  'start' => '2014-05-25',
-  'end'   => '2014-06-20'
+  'label' => 'Wishlist Pages (2)',
+  'start' => '2014-4-07',
+  'end'   => '2014-5-07'
 );
 
 $front[] = array(
-  'label' => 'Project 4',
-  'start' => '2014-05-06',
-  'end'   => '2014-06-17',
-  'class' => 'important',
+  'label' => 'User Accounts Pages (3)',
+  'start' => '2014-5-01',
+  'end'   => '2014-6-01'
 );
 
 $front[] = array(
-  'label' => 'Project 5',
-  'start' => '2014-05-11',
-  'end'   => '2014-06-03',
-  'class' => 'urgent',
+  'label' => 'Further Functionality',
+  'start' => '2014-6-01',
+  'end'   => '2014-7-01'
 );
 
 $front[] = array(
-  'label' => 'Project 6',
-  'start' => '2014-05-15',
-  'end'   => '2014-07-03'
-);
-
-$front[] = array(
-  'label' => 'Project 7',
-  'start' => '2014-06-01',
-  'end'   => '2014-07-03',
-  'class' => 'important',
-);
-
-$front[] = array(
-  'label' => 'Project 8',
-  'start' => '2014-06-01',
-  'end'   => '2014-08-05'
-);
-
-$front[] = array(
-  'label' => 'Project 9',
-  'start' => '2014-07-22',
-  'end'   => '2014-09-05',
-  'class' => 'urgent',
+  'label' => 'Merging with AngularJS',
+  'start' => '2014-6-20',
+  'end'   => '2014-7-20'
 );
 
 ?>
