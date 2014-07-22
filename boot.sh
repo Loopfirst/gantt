9fb776e09db98777901df30b7e3a3e33602e7b46
Kevin Woo <kevinawoo@gmail.com>
2014-07-22 09:57:26 -0500

    don't compress and uncompress

--- boot.sh
+++ boot.sh
@@ -4,7 +4,7 @@
 cd $1
 
 # untar data folder
-echo
-echo "Uncompressing files"
-tar -xzvf data.tar.gz
-tar -xzvf styles.tar.gz
+# echo
+# echo "Uncompressing files"
+# tar -xzvf data.tar.gz
+# tar -xzvf styles.tar.gz
